beb077084e55985030f3b0fd9d84d3e6d433a56c
Abiram Nair <abiramrnair@gmail.com>
2020-09-28 17:55:23 -0400

    update 1.9
    implemented mergesort, no visible bugs

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ for (i = 0; i < 5; i++) {
 slider.oninput = function() {
     slidevalue.innerHTML = this.value;
     numBoxes = this.value;    
-    var obj = document.getElementsByClassName("box");
+    var obj = document.getElementsByClassName("box");    
    
     while(obj[0]) {
         obj[0].parentNode.removeChild(obj[0]);
@@ -69,9 +69,9 @@ function removePx (str) {
 
 
 async function Swap(x, y) {
-    if (sortSelection = 0) {
+    if (sortSelection == 0) {
     await sleep((-22)*(myCollection.length) + 1120);
-    } else if (sortSelection = 1) {
+    } else if (sortSelection == 1) {
         await sleep(25);
     }
     var temp = myCollection[x].style.height;
@@ -159,7 +159,91 @@ function doQuickSort() {
     QuickSort(0, myCollection.length - 1);    
 }
 
+
 // MergeSort 
+
+async function merge(l, m, r) { 
+        await sleep(200);
+        // Find sizes of two subarrays to be merged 
+        var n1 = m - l + 1; 
+        var n2 = r - m;        
+        
+        
+
+        /* Create temp arrays */
+        let leftArray = new Array(n1);
+        let rightArray = new Array(n2); 
+  
+        /*Copy data to temp arrays*/
+        for (i = 0; i < n1; ++i) {                     
+            leftArray[i] = removePx(myCollection[l + i].style.height);                        
+        }
+        
+        for (j = 0; j < n2; ++j) {            
+            rightArray[j] = removePx(myCollection[m + 1 + j].style.height);
+             
+        }
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays 
+        var i = 0
+        var j = 0; 
+  
+        // Initial index of merged subarry array 
+        var k = l; 
+
+        while ((i < n1) && (j < n2)) {             
+            if (leftArray[i] >= rightArray[j]) {                                              
+                myCollection[k].style.height = addPx(leftArray[i]);                              
+                i++; 
+                
+            } else {
+                
+                myCollection[k].style.height = addPx(rightArray[j]); 
+                
+                j++; 
+            }
+            
+            k++; 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {                  
+            myCollection[k].style.height = addPx(leftArray[i]);            
+            i++; 
+            k++;                   
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {                      
+            myCollection[k].style.height = addPx(rightArray[j]); 
+            j++; 
+            k++;
+                       
+        }   
+        
+        
+}
+
+async function MergeSort(l, r) {
+    await sleep(50);
+    if (l < r) {
+        myCollection[l].style.background = "red";                 
+        var m = Math.floor((l + r) / 2);          
+        await MergeSort(l, m);        
+        await MergeSort(m + 1, r);
+        myCollection[r].style.background = "green";
+        await merge(l, m, r);
+        myCollection[l].style.background = "cyan";
+        myCollection[r].style.background = "cyan";         
+    }
+}
+
+function doMergeSort() {
+    sortSelection = 1;
+    MergeSort(0, myCollection.length - 1);
+}
 
 
 
